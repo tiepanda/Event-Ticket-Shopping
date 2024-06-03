e2da79c159b5fa8c3c54231a39ad92f92d282490
Prajwal R Koushik <Prajwalk@talisma.com>
2024-06-03 12:52:07 +0530

    last commit of the project

--- BookShoppingCartMvcUI/Controllers/ReportsController.cs
+++ BookShoppingCartMvcUI/Controllers/ReportsController.cs
@@ -11,19 +11,21 @@ public class ReportsController : Controller
     // GET: ReportsController
     public async Task<ActionResult> TopFiveSellingTickets(DateTime? sDate = null, DateTime? eDate = null)
     {
-        try
-        {
             // by default, get last 7 days record
             DateTime startDate = sDate ?? DateTime.UtcNow.AddDays(-7);
             DateTime endDate = eDate ?? DateTime.UtcNow;
             var topFiveSellingTickets = await _reportRepository.GetTopNSellingTicketsByDate(startDate, endDate);
             var vm = new TopNSoldTicketsVm(startDate, endDate, topFiveSellingTickets);
             return View(vm);
-        }
-        catch (Exception ex)
-        {
-            TempData["errorMessage"] = "Something went wrong";
-            return RedirectToAction("Index", "Home");
-        }
+        //try
+        //{
+
+        //}
+        
+        //catch (Exception ex)
+        //{
+        //    TempData["errorMessage"] = "Something went wrong";
+        //    return RedirectToAction("Index", "Home");
+        //}
     }
 }
--- BookShoppingCartMvcUI/Program.cs
+++ BookShoppingCartMvcUI/Program.cs
@@ -17,17 +17,17 @@ builder.Services
     .AddDefaultUI()
     .AddDefaultTokenProviders();
 builder.Services.AddControllersWithViews();
-builder.Services.AddScoped<IHomeRepository, HomeRepository>();
-builder.Services.AddScoped<ICartRepository, CartRepository>();
-builder.Services.AddScoped<IUserOrderRepository, UserOrderRepository>();
-builder.Services.AddScoped<IStockRepository, StockRepository>();
-builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
-builder.Services.AddScoped<IFileService, FileService>();
-builder.Services.AddScoped<ITicketRepository, TicketRepository>();
-builder.Services.AddScoped<IReportRepository, ReportRepository>();
+builder.Services.AddTransient<IHomeRepository, HomeRepository>();
+builder.Services.AddTransient<ICartRepository, CartRepository>();
+builder.Services.AddTransient<IUserOrderRepository, UserOrderRepository>();
+builder.Services.AddTransient<IStockRepository, StockRepository>();
+builder.Services.AddTransient<ICategoryRepository, CategoryRepository>();
+builder.Services.AddTransient<IFileService, FileService>();
+builder.Services.AddTransient<ITicketRepository, TicketRepository>();
+builder.Services.AddTransient<IReportRepository, ReportRepository>();
 
 var app = builder.Build();
-// Uncomment it when you run the project first time, It will registered an admin
+// Uncomment it when you run the project when u want registered an admin
 //using (var scope = app.Services.CreateScope())
 //{
 //    await DbSeeder.SeedDefaultData(scope.ServiceProvider);
